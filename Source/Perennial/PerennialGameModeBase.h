5da49baa7c1c938e62c9981e5af66dc792746ff6
bonbombs <kelly.zhang2306@gmail.com>
2017-03-20 19:32:52 -0400

    Initial commit

---
+++ Source/Perennial/PerennialGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PerennialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PERENNIAL_API APerennialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
