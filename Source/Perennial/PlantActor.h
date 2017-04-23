a85c8f3b54c4cce71b21fe84b1d356da46660827
Hope Wallace <wallacehope1@gmail.com>
2017-04-23 09:53:18 -0400

    Pretty sure I added more controller vibrations, can't check right now though

--- Source/Perennial/PlantActor.h
+++ Source/Perennial/PlantActor.h
@@ -77,8 +77,12 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Is Watered")
 	bool bIsWatered;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Is Fertilized")
 	bool bIsFertilized;
+
 	bool bIsHarvestable;
 
 	int Quality;
